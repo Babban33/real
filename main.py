42d55b4d9f66bc2b07832e3229f96ce8e2b8f610
Babban33 <chaitanyawankhede705@gmail.com>
2024-02-29 12:39:53 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
 import cv2
-import streamlit as st
 import numpy as np
+import base64
+import tensorflow as tf
 import pyrealsense2 as rs
-import pandas  as pd
 import mediapipe as mp
-def main():
-    st.title("BIAC AGC Project")
-    video = st.empty()
+import pandas as pd
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ----------------- Opening -----------------#
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
     pipeline = rs.pipeline()
     config = rs.config()
     config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
     config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
-    data = pd.read_csv('./sheet.csv')
+    data = pd.read_csv("./app/sheet.csv")
     pipeline.start(config)
     mp_face_mesh = mp.solutions.face_mesh
     face_mesh = mp_face_mesh.FaceMesh()
@@ -20,6 +34,7 @@ def main():
             frames = pipeline.wait_for_frames()
             depth_frame = frames.get_depth_frame()
             color_frame = frames.get_color_frame()
+
             if not depth_frame or not color_frame:
                 continue
 
@@ -28,6 +43,7 @@ def main():
 
             rgb_frame = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
             results = face_mesh.process(rgb_frame)
+
             if results.multi_face_landmarks:
                 for face_landmarks in results.multi_face_landmarks:
                     upper_lip_center = tuple(
@@ -36,7 +52,8 @@ def main():
                     lower_lip_center = tuple(
                         map(int, (face_landmarks.landmark[14].x * color_image.shape[1], face_landmarks.landmark[14].y * color_image.shape[0]))
                     )
-                    cv2.circle(color_image, upper_lip_center, 5, (0, 0, 255), -1)
+
+                    cv2.circle(color_image, upper_lip_center, 5, (0, 255, 0), -1)
                     cv2.circle(color_image, lower_lip_center, 5, (0, 0, 255), -1)
 
                     distance = np.linalg.norm(np.array(upper_lip_center) - np.array(lower_lip_center))
@@ -49,11 +66,19 @@ def main():
                     actual_length = distance * multiplication_factor
                     actual_length = actual_length * 10
 
-                    text = f"Upper Lip Depth: {upper_lip_depth} mm, Lower Lip Depth: {lower_lip_depth} mm, Actual Length: {actual_length} mm"
+                    # Add text to the image
+                    text = f"Upper Lip Depth: {upper_lip_depth} m\nLower Lip Depth: {lower_lip_depth} m\nActual Length: {actual_length} units"
                     cv2.putText(color_image, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-                    video.image(color_image, channels="BGR")
-    except:
-        pass
+                    
+                    # Encoding image
+                    _, image_buffer = cv2.imencode('.jpg', color_image)
+                    image_bytes = base64.b64encode(image_buffer).decode('utf-8')
+                    await websocket.send_text(image_bytes)
+    except WebSocketDisconnect:
+        await websocket.send_json({"actual_length": actual_length})
+        pipeline.stop()
+        face_mesh.close()
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app)
